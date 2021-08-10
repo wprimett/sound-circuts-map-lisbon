0dbadb9ae9a568880ffce963756ba2485ec87351
William Primett <williamprimett@Williams-MacBook-Pro.local>
2021-08-10 19:55:50 +0100

    teusday afternnon jam

--- public/js/lisbon_map.js
+++ public/js/lisbon_map.js
@@ -8,7 +8,7 @@ var map = L.map('map').setView([43.659752, -79.378161], 20);
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 21
 }).addTo(map);
